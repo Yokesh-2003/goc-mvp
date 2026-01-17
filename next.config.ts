d6ff00ae25d18edd303ff98362a3153e542ee214
Yokesh-2003 <vanuvanu21342@gmail.com>
2026-01-17 18:03:07 +0530

    fix tiktok site verification file serving

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/tiktokirROGV9y9sLA4RlziGbK5t193mJvOorz.txt",
+        destination: "/tiktokirROGV9y9sLA4RlziGbK5t193mJvOorz.txt",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
