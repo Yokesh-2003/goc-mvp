f9ee77cc23ba3e34d0d255261e8dc6f24999efd5
Yokesh-2003 <vanuvanu21342@gmail.com>
2026-01-17 18:05:40 +0530

    restore default next config for tiktok verification

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/tiktokirROGV9y9sLA4RlziGbK5t193mJvOorz.txt",
-        destination: "/tiktokirROGV9y9sLA4RlziGbK5t193mJvOorz.txt",
-      },
-    ];
-  },
+  reactStrictMode: true,
 };
 
 export default nextConfig;
